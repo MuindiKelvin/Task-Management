3ae311142dd9547cec61f6ce0f4af626a6101018
MuindiKelvin <kevinmuindi10@students.uonbi.ac.ke>
2021-12-06 13:33:23 +0300

    Initial commit

---
+++ tms_db.sql
@@ -0,0 +1,182 @@
+-- phpMyAdmin SQL Dump
+-- version 4.5.1
+-- http://www.phpmyadmin.net
+--
+-- Host: 127.0.0.1
+-- Generation Time: Dec 06, 2021 at 11:30 AM
+-- Server version: 10.1.13-MariaDB
+-- PHP Version: 5.6.20
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `tms_db`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `project_list`
+--
+
+CREATE TABLE `project_list` (
+  `id` int(40) NOT NULL,
+  `name` varchar(200) NOT NULL,
+  `description` text NOT NULL,
+  `status` tinyint(2) NOT NULL,
+  `start_date` date NOT NULL,
+  `end_date` date NOT NULL,
+  `manager_id` int(30) NOT NULL,
+  `user_ids` text NOT NULL,
+  `date_created` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `system_settings`
+--
+
+CREATE TABLE `system_settings` (
+  `id` int(30) NOT NULL,
+  `name` text NOT NULL,
+  `email` varchar(200) NOT NULL,
+  `contact` varchar(20) NOT NULL,
+  `address` text NOT NULL,
+  `cover_img` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `system_settings`
+--
+
+INSERT INTO `system_settings` (`id`, `name`, `email`, `contact`, `address`, `cover_img`) VALUES
+(1, 'TASK MANAGEMENT ', 'info@tms.com', '+85 874 456 671', 'Crest Road, Ladhies, Glasgow, 1767', '');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `task_list`
+--
+
+CREATE TABLE `task_list` (
+  `id` int(11) NOT NULL,
+  `project_id` int(30) NOT NULL,
+  `task` varchar(200) NOT NULL,
+  `description` text NOT NULL,
+  `status` tinyint(4) NOT NULL,
+  `date_created` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `id` int(30) NOT NULL,
+  `firstname` varchar(200) NOT NULL,
+  `lastname` varchar(200) NOT NULL,
+  `email` varchar(200) NOT NULL,
+  `password` text NOT NULL,
+  `type` tinyint(4) NOT NULL COMMENT '''1 = admin, 2 = staff''',
+  `avatar` text NOT NULL COMMENT 'no-image-available.png',
+  `date_created` datetime NOT NULL DEFAULT '0000-00-00 00:00:00' ON UPDATE CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`id`, `firstname`, `lastname`, `email`, `password`, `type`, `avatar`, `date_created`) VALUES
+(1, 'TMS Admin', '', 'tmsadmin@gmail.com', '74b4893e0959da930173a537fe223e44', 1, '1638304620_1638089460_1605855720_avatar.jpg', '2021-12-04 23:59:45');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user_productivity`
+--
+
+CREATE TABLE `user_productivity` (
+  `id` int(30) NOT NULL,
+  `project_id` int(30) NOT NULL,
+  `task_id` int(30) NOT NULL,
+  `comment` text NOT NULL,
+  `subject` varchar(200) NOT NULL,
+  `date` date NOT NULL,
+  `start_time` time NOT NULL,
+  `end_time` time NOT NULL,
+  `user_id` int(30) NOT NULL,
+  `time_rendered` float NOT NULL,
+  `date_created` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `project_list`
+--
+ALTER TABLE `project_list`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `system_settings`
+--
+ALTER TABLE `system_settings`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `task_list`
+--
+ALTER TABLE `task_list`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `user_productivity`
+--
+ALTER TABLE `user_productivity`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `project_list`
+--
+ALTER TABLE `project_list`
+  MODIFY `id` int(40) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `task_list`
+--
+ALTER TABLE `task_list`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(30) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+--
+-- AUTO_INCREMENT for table `user_productivity`
+--
+ALTER TABLE `user_productivity`
+  MODIFY `id` int(30) NOT NULL AUTO_INCREMENT;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
