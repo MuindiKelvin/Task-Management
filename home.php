1b11932c77fe3af0abe00672f5a5c65a3fe58d5e
MuindiKelvin <kevinmuindi10@students.uonbi.ac.ke>
2021-12-06 13:47:55 +0300

    Initial commit

--- home.php
+++ home.php
@@ -22,7 +22,7 @@ if($_SESSION['login_type'] != 1)
     ?>
         
       <div class="row">
-        <div class="col-md-10">
+        <div class="col-md-12">
         <div class="card">
           <div class="card-header">
             <b>Projects Overall Analysis</b>
@@ -121,7 +121,7 @@ if($_SESSION['login_type'] != 1)
         </div>
         </div>
 
-        <div class="col-md-2">
+        <!--<div class="col-md-2">
           <div class="row">
           <div class="col-12 col-sm-6 col-md-12">
             <div class="small-box bg-light shadow-sm border">
@@ -148,4 +148,4 @@ if($_SESSION['login_type'] != 1)
           </div>
       </div>
         </div>
-      </div>
+      </div>-->
